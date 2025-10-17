47118f7958987af1de736570a10a3ccb3e259f7a
Raz <razmikparzyan@gmail.com>
2025-10-17 16:39:25 +0400

    containerization setup

--- trainer-workload/src/main/java/com/company/trainerworkload/dao/TrainerSummaryRepository.java
+++ trainer-workload/src/main/java/com/company/trainerworkload/dao/TrainerSummaryRepository.java
@@ -1,10 +1,12 @@
 package com.company.trainerworkload.dao;
 
 import com.company.trainerworkload.entity.TrainerSummary;
+import org.springframework.context.annotation.Profile;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 import java.util.Optional;
 
+@Profile("!no-integration")
 public interface TrainerSummaryRepository extends MongoRepository<TrainerSummary, String> {
     Optional<TrainerSummary> findByTrainerUsername(String username);
 }
